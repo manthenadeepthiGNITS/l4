143b3d58b09199dbb86974685e0e9d8e6e386d67
manthenadeepthi <manthena.deepthi@gnits.ac.in>
2022-11-19 22:31:59 +0530

    Add iint staged

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,13 +1,36 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 describe("Todoist test suite", () => {
   beforeAll(() => {
     // const today = new Date();
+    const formattedDate = (d) => {
+      return d.toISOString().split("T")[0];
+    };
+
+    var dateToday = new Date();
+    const today = formattedDate(dateToday);
+    const yesterday = formattedDate(
+      new Date(new Date().setDate(dateToday.getDate() - 1))
+    );
+    const tomorrow = formattedDate(
+      new Date(new Date().setDate(dateToday.getDate() + 1))
+    );
+
     add({
-      title: "todo test",
+      title: "Pay rent",
+      dueDate: today,
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    add({
+      title: "Submit assignment",
+      dueDate: yesterday,
+      completed: false,
+    });
+    add({
+      title: "File taxes",
+      dueDate: tomorrow,
+      completed: false,
     });
   });
   test("should add new todo", () => {
@@ -23,5 +46,16 @@ describe("Todoist test suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("Should retrieve overdue items", () => {
+    expect(overdue().length).toEqual(1);
+  });
+
+  test("Should retrieve due today items", () => {
+    expect(dueToday().length).toEqual(2);
+  });
+
+  test("Should retrieve due later items", () => {
+    expect(dueLater().length).toEqual(1);
   });
 });
